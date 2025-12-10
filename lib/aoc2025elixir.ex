9ef551ba54746519da754caef260ad852b685984
Valentin Rochet <val.rochet@gmail.com>
2025-12-10 15:19:03 +0100

    init project 2025

---
+++ lib/aoc2025elixir.ex
@@ -0,0 +1,18 @@
+defmodule Aoc2025elixir do
+  @moduledoc """
+  Documentation for `Aoc2025elixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc2025elixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aoc2025elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc2025elixirTest do
+  use ExUnit.Case
+  doctest Aoc2025elixir
+
+  test "greets the world" do
+    assert Aoc2025elixir.hello() == :world
+  end
+end
